baefdffec372847aa8dda05bd715100334d640ae
d <d>
2020-01-23 21:22:26 +0900

    更新

---
+++ SoundTest/Assets/SoundEditor4/Script/EnemyModeGroup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyModeGroup : MonoBehaviour
+{
+    public static GameObject obj { get; private set; }
+    // Start is called before the first frame update
+    void Awake()
+    {
+        obj = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ SoundTest/Assets/SoundEditor4/Script/PlModeGroup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlModeGroup : MonoBehaviour
+{
+    public static GameObject obj { get; private set; }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        obj = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
